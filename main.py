6808b8058834e88baefd93b1897b049b669a2a82
dontpanic123 <42323006+dontpanic123@users.noreply.github.com>
2025-10-02 11:22:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, requests, datetime
 
 API_KEY = os.getenv("OPENWEATHER_KEY")  # OpenWeather API Key
-CITY = "Beijing"
+CITY = "Sydney"
 
 # 请求天气
 url = f"http://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={API_KEY}&lang=zh_cn&units=metric"
